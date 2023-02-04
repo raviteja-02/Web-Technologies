2b831d86e40be079068ca144d11cae80b24f86d3
raviteja-02 <raviteja-02@users.noreply.github.com>
2023-02-04 21:42:09 +0530

    MySQL

---
+++ MySQL/mysql.php
@@ -0,0 +1,40 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "20-5a9";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+echo "Connected successfully<br>";
+
+// Create table
+$sql = "CREATE TABLE users (
+username VARCHAR(30) ,
+email VARCHAR(50),
+password VARCHAR(30) 
+)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table created successfully<br>";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
+// Insert data
+$sql = "INSERT INTO users VALUES ('john', 'john@example.com', 'secret')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully<br>";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
